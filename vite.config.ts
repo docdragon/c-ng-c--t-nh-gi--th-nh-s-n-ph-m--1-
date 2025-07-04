0008b697c0eb0a5e0dad1d541ce4792ff50f1558
docdragon <nguyenminhthien.pr98@gmail.com>
2025-07-04 16:49:02 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // Khối 'define' đã được loại bỏ để sử dụng cách xử lý biến môi trường tiêu chuẩn của Vite.
-})
+/// <reference types="vite/client" />
